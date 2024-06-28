4d8bbe8f95a7e792aac71676cc1376756bac5f50
souravq <souravbera515@gmail.com>
2024-06-28 22:44:56 +0530

    code editor component added

---
+++ src/components/CodeEditor.tsx
@@ -0,0 +1,36 @@
+import React, { useState, ChangeEvent } from "react";
+import Prism from "prismjs";
+import "prismjs/themes/prism.css";
+//import "./CodeEditor.css";
+
+const CodeEditor: React.FC = () => {
+  const [code, setCode] = useState<string>("");
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setCode(e.target.value);
+  };
+
+  return (
+    <div className="code-editor-container">
+      <textarea
+        value={code}
+        onChange={handleChange}
+        className="code-input"
+        spellCheck="false"
+      />
+      <pre className="code-output">
+        <code
+          dangerouslySetInnerHTML={{
+            __html: Prism.highlight(
+              code,
+              Prism.languages.javascript,
+              "javascript"
+            ),
+          }}
+        />
+      </pre>
+    </div>
+  );
+};
+
+export default CodeEditor;
